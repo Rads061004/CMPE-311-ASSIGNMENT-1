64885e638464308f1047c8b75c6d8565a0e90f6d
Rads061004 <armaanm1@umbc.edu>
2025-10-08 15:37:22 -0400

    Update fsm.vhd

--- fsm.vhd
+++ fsm.vhd
@@ -1,1 +1,11 @@
+BEGIN
 
+      PROCESS (CLK, RESET)
+      BEGIN
+      IF RESET = '1' THEN
+            Current_State <= S_IDLE;
+            State_Count <= 0;
+            BUSY_OUT      <= '0'; 
+
+      ELSIF rising_edge(CLK) THEN
+  
