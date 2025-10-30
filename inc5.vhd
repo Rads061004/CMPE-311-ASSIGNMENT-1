c694010ca2b34022dc3586cb22812f694c99e891
Rads061004 <armaanm1@umbc.edu>
2025-10-30 16:59:37 -0400

    Refactor inc5.vhd to improve formatting and structure

--- inc5.vhd
+++ inc5.vhd
@@ -10,26 +10,13 @@ end inc5;
 
 architecture structural of inc5 is
     component adder5
-        port (
-            a, b : in  STD_LOGIC_VECTOR(4 downto 0);
-            cin  : in  STD_LOGIC; 
-            sum  : out STD_LOGIC_VECTOR(4 downto 0);
-            cout : out STD_LOGIC
-        );
+        port (a, b : in STD_LOGIC_VECTOR(4 downto 0); cin : in STD_LOGIC; 
+              sum : out STD_LOGIC_VECTOR(4 downto 0); cout : out STD_LOGIC);
     end component;
-
-    signal one         : STD_LOGIC_VECTOR(4 downto 0);
-    signal cin_sig     : STD_LOGIC;
+    
+    signal one : STD_LOGIC_VECTOR(4 downto 0);
     signal cout_unused : STD_LOGIC;
 begin
-    one     <= "00001";  -- signal for b
-    cin_sig <= '0';      -- signal for cin
-
-    u_add: adder5 port map (
-        a    => a,
-        b    => one,
-        cin  => cin_sig,
-        sum  => inc,
-        cout => cout_unused
-    );
+    one <= "00001";
+    u_add: adder5 port map (a => a, b => one, cin => '0', sum => inc, cout => cout_unused);
 end structural;
